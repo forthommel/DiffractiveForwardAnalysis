c6ad3b2f33b6a1798895e6d4d06ca03fc7c399d9
Laurent <laurent.forthomme@cern.ch>
2016-09-06 14:39:31 +0200

    Forgot a file

---
+++ GammaGammaLeptonLepton/interface/PhotonVertexSelector.h
@@ -0,0 +1,18 @@
+#ifndef PhotonVertexSelector_h
+#define PhotonVertexSelector_h
+
+class PhotonVertexSelector
+{
+ public:
+  inline PhotonVertexSelector() {;}
+  inline ~PhotonVertexSelector() {;}
+
+  inline void SetEventHandles() {
+    
+  }
+
+ private:
+  
+};
+
+#endif
